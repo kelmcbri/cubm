4afa08e820426f1e2423b118d51330b287a530b3
Keller McBride <keller.mcbride@gmail.com>
2017-05-24 12:25:44 -0500

    v2.3 cubm-eem.tcl output queing

--- cubm-eem.tcl
+++ cubm-eem.tcl
@@ -3,7 +3,11 @@
 namespace import ::cisco::eem::*
 namespace import ::cisco::lib::*
 
-# 1. query the information of latest triggered eem event
+# This is cubm-eem.tcl version 2
+# Mechanism has been added to meter out messages to Cloverleaf every 3 seconds
+# (There is come issue that keeps Cloverleaf from accepting messages sent close together)
+
+# Query the information of latest triggered eem event
 array set arr_einfo [event_reqinfo]
 if {$_cerrno != 0} {
     set result [format "component=%s; subsys err=%s; posix err=%s;\n%s" \
@@ -19,14 +23,34 @@ set port ""
 
 regexp {.*%CUBM% (.*?):(.*?) \((.*?)\)} $msg match ip port msg
 
+#Set lastTimeSnet to zero in case we haven't saved the value before and read the previous value
+set lastTimeSent 0
+set getLastTimeSent [appl_reqinfo key "showLastTimeSent"]
+set nowTime [clock seconds]
 
+#If the first element in the list is 'data', then the second element is the value we need
+if {[ lindex $getLastTimeSent 0 ] == "data" } {
+    set lastTimeSent [ lindex $getLastTimeSent 1 ]
+}
+
+# If the current time is less than three seconds from the last message sent, wait 3 seconds
+if { [expr $nowTime - $lastTimeSent] < 3 } {
+    puts -nonewline "\r Waiting 3000 seconds.\r"
+    after 3000
+}
+
+#Now send the message to Cloverleaf
 if { [catch {set sd [socket $ip $port]}] } {
    puts -nonewline stderr "        Connection to Cloverleaf at $ip : $port has Failed ***\n\r"
    exit 1
  } else {
-    puts -nonewline "$msg sent to Cloverleaf at $ip : $port\r"
+    puts -nonewline "\r $msg sent to Cloverleaf at $ip : $port\r"
     puts -nonewline $sd [string trim $msg]
     puts -nonewline $sd "\r"
     flush $sd
     close $sd
 }
+
+#Update the last time sent value to current time for the next policy instance
+set lastTimeSent [clock seconds]
+appl_setinfo key "showLastTimeSent" data $lastTimeSent
